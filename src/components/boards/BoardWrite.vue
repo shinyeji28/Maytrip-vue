e3dabcb6429b400ec99c7c415975a1a5f0dc6b5c
shinyeji28 <xntm10@naver.com>
2023-11-20 11:51:40 +0900

    refactor: board-write 부모 컴포넌트 css적용을 위해 기존 css제거

--- src/components/boards/BoardWrite.vue
+++ src/components/boards/BoardWrite.vue
@@ -8,9 +8,7 @@ import { listSido, listGugun } from "@/api/sidoGugun.js";
 const form = ref(null);
 
 const fileInput = ref(null);
-const rules = ref([
-  value => !!value || '입력해주세요.'
-]);
+const rules = ref([(value) => !!value || "입력해주세요."]);
 
 const today = new Date();
 today.setHours(0, 0, 0, 0);
@@ -72,143 +70,88 @@ const handleFileUpload = () => {
   console.log(selectedFile.value);
 };
 
-const onSubmit = async() => {
+const onSubmit = async () => {
   const formData = new FormData(form.value);
-  formData.append('memberId', 1);     // todo token으로 수정
-  formData.append('startDate', startDate.value.toISOString().substring(0,10));
-  formData.append('endDate', endDate.value.toISOString().substring(0,10));
-  formData.append('content', content.value);
+  formData.append("memberId", 1); // todo token으로 수정
+  formData.append("startDate", startDate.value.toISOString().substring(0, 10));
+  formData.append("endDate", endDate.value.toISOString().substring(0, 10));
+  formData.append("content", content.value);
   // for (let key of formData.keys()) {
-	//   console.log(key, ":", formData.get(key));
+  //   console.log(key, ":", formData.get(key));
   // }
-   await saveBoard(formData);
+  await saveBoard(formData);
 };
 
 const setStartDate = () => {
-  if(startDate.value>endDate.value){
+  if (startDate.value > endDate.value) {
     endDate.value = startDate.value;
   }
 };
-const setEndDate = () => {
-};
-
+const setEndDate = () => {};
 </script>
 
 <template>
-  <div class="container">
-    <div class="banner">
-      <img src="@/assets/images/trip4.jpg" />
-      <div class="title">여행 크루 모집을 위한 글을 작성하세요 😎</div>
-    </div>
-    <main class="container">
-      <div class="main-wrap">
-        <div>   
-          <form @submit.prevent="onSubmit" ref="form">
-            <v-text-field
-              label="모집 글 제목"
-              name="title"
-              :rules="rules"
-              hide-details="auto"
-            ></v-text-field>
-            <v-file-input
-              label="썸네일 이미지를 넣어주세요"
-              v-model="selectedFile"
-              @change="handleFileUpload"
-              name="image"
-              :rules="rules"
-              hide-details="auto"
-            ></v-file-input>
-            <v-text-field
-              label="모집 인원수"
-              name="headcount"
-              :rules="rules"
-              hide-details="auto"
-            ></v-text-field>
-            <v-select
-            v-model="selectedSido"
-            :items="sido"
-            item-title="sidoName"
-            item-value="sidoCode"
-            label="시도 선택"
-            return-object
-            hint="구군까지 선택해주세요"
-            name="sidoCode"
-          ></v-select>
-          <v-select
-            v-model="selectedGugun"
-            :items="gugun"
-            item-title="gugunName"
-            item-value="gugunCode"
-            label="구군 선택"
-            return-object
-            name="gugunCode"
-          ></v-select>
+  <!-- <div class="title">여행 크루 모집을 위한 글을 작성하세요 😎</div> -->
 
-            <v-date-picker v-model="startDate" :min="today" label="여행 출발 날짜" @click="setStartDate"></v-date-picker>
-            <v-date-picker v-model="endDate" :min="startDate" label="여행 종료 날짜" @click="setEndDate"></v-date-picker>
+  <div>
+    <form @submit.prevent="onSubmit" ref="form">
+      <v-text-field
+        label="모집 글 제목"
+        name="title"
+        :rules="rules"
+        hide-details="auto"
+      ></v-text-field>
+      <v-file-input
+        label="썸네일 이미지를 넣어주세요"
+        v-model="selectedFile"
+        @change="handleFileUpload"
+        name="image"
+        :rules="rules"
+        hide-details="auto"
+      ></v-file-input>
+      <v-text-field
+        label="모집 인원수"
+        name="headcount"
+        :rules="rules"
+        hide-details="auto"
+      ></v-text-field>
+      <v-select
+        v-model="selectedSido"
+        :items="sido"
+        item-title="sidoName"
+        item-value="sidoCode"
+        label="시도 선택"
+        return-object
+        hint="구군까지 선택해주세요"
+        name="sidoCode"
+      ></v-select>
+      <v-select
+        v-model="selectedGugun"
+        :items="gugun"
+        item-title="gugunName"
+        item-value="gugunCode"
+        label="구군 선택"
+        return-object
+        name="gugunCode"
+      ></v-select>
 
-            <VQuillEditor :content="content" @update:modelValue="changeEditor" />
-            <v-btn type="submit">제출</v-btn>
-          </form>
-        </div>
-      </div>
-    </main>
+      <v-date-picker
+        v-model="startDate"
+        :min="today"
+        label="여행 출발 날짜"
+        @click="setStartDate"
+      ></v-date-picker>
+      <v-date-picker
+        v-model="endDate"
+        :min="startDate"
+        label="여행 종료 날짜"
+        @click="setEndDate"
+      ></v-date-picker>
+
+      <VQuillEditor :content="content" @update:modelValue="changeEditor" />
+      <v-btn type="submit">제출</v-btn>
+    </form>
   </div>
 </template>
 
-<style scoped>
-.container {
-  display: flex;
-
-  margin: auto;
-  padding: 0;
-
-  flex-direction: column;
-  max-width: 1080px;
-}
-.banner {
-  /* position: fixed; */
-  position: absolute;
-  left: 0;
-  top: 70px;
-  width: 100%;
-  height: 400px;
-  z-index: -1;
-}
-.banner img {
-  object-fit: cover;
-  width: 100%;
-  height: 100%;
-  box-shadow: 0px 8px 15px 0px rgba(0, 0, 0, 0.3);
-}
-.title {
-  position: absolute;
-  top: 35%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 0;
-  font-size: 30px;
-  font-weight: 900;
-  text-shadow: 2px 2px 4px rgba(255, 255, 255, 0.6);
-}
-
-.container {
-  width: 100%;
-  height: 100%;
-}
-.main-wrap {
-  margin-top: 250px;
-  margin-bottom: 0;
-  min-height: 700px;
-  background-color: #fff;
-  border-radius: 10px 10px 0 0;
-  box-shadow: -4px 0px 4px 0px rgba(0, 0, 0, 0.25),
-    4px 4px 4px 0px rgba(0, 0, 0, 0.25);
-
-  padding: 40px;
-  overflow: scroll;
-}
-.main-wrap::-webkit-scrollbar {
-  display: none; /* Chrome, Safari, Opera */
-}
-</style>
+<style scoped></style>
