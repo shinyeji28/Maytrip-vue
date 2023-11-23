b7a7bf92546c31f1e374912ddc368183f7af2cdd
shinyeji28 <xntm10@naver.com>
2023-11-23 09:47:49 +0900

    refactor: 주석 제거

--- src/components/review/item/review.vue
+++ src/components/review/item/review.vue
@@ -8,31 +8,33 @@ const props = defineProps({
 const review = ref({});
 
 onMounted(async () => {
-    review.value = props.review;
+  review.value = props.review;
 });
 </script>
 
 <template>
-    <v-container>
-        <v-card class="pa-8">
-        <!-- 리뷰 제목 -->
-        <v-card-title class="headline mb-6">{{ review.title }}</v-card-title>
-        
-        <!-- 리뷰 내용 -->
-        <v-card-text class="body-1">{{ review.content }}</v-card-text>
-        
-        <!-- 리뷰 사진들 -->
-        <v-row>
-            <v-col v-for="(photo, idx) in review.images" :key="idx" cols="12" lg="4">
-            <v-hover v-slot:default="{ hover }">
-                <v-card :elevation="hover ? 12 : 2" class="pa-2">
-                <v-img :src="photo.url" class="transition-img" contain></v-img>
-                </v-card>
-            </v-hover>
-            </v-col>
-        </v-row>
-        </v-card>
-    </v-container>
+  <v-container>
+    <v-card class="pa-8">
+      <v-card-title class="headline mb-6 text">{{ review.title }}</v-card-title>
+
+      <v-card-text class="body-1 text">{{ review.content }}</v-card-text>
+
+      <v-row>
+        <v-col
+          v-for="(photo, idx) in review.images"
+          :key="idx"
+          cols="12"
+          lg="4"
+        >
+          <v-hover v-slot:default="{ hover }">
+            <v-card :elevation="hover ? 12 : 2" class="pa-2">
+              <v-img :src="photo.url" class="transition-img" contain></v-img>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <style scoped>
@@ -42,5 +44,8 @@ onMounted(async () => {
 
 .transition-img:hover {
   transform: scale(1.1);
+}
+.text {
+  font-weight: 700;
 }
 </style>
