d1fb7dc41633de7a2f3a374ee2c08c05b60926b0
shinyeji28 <xntm10@naver.com>
2023-11-23 16:04:13 +0900

    refactor: main 디자인 변경

--- src/views/TheMainView.vue
+++ src/views/TheMainView.vue
@@ -1,32 +1,91 @@
 <script setup>
-import { ref } from "vue";
-const items = ref([
-  { text: "1번 그림", src: "https://cdn.vuetifyjs.com/images/cards/docks.jpg" },
-  { text: "2번 그림", src: "https://cdn.vuetifyjs.com/images/cards/hotel.jpg" },
-  {
-    text: "3번 그림",
-    src: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-  },
-]);
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { getReviewList } from "@/api/review.js";
+import { listBoard } from "@/api/board.js";
+
+const router = useRouter();
+
+const reviews = ref([]);
+const items = ref([]);
+
+const getReview = async () => {
+  const { data } = await getReviewList();
+  reviews.value = data.reverse().slice(0, 3);
+};
+
+const getBoard = async () => {
+  const { data } = await listBoard();
+  items.value = data.reverse().slice(0, 10);
+  items.value = data.reverse().slice(0, 3);
+  console.log(items.value);
+  console.log("getBoard 완료:", items.value);
+};
+
+getReview();
+getBoard();
+
+const slider = ref(["First", "Second", "Third", "Fourth", "Fifth"]);
 </script>
 
 <template>
   <div class="container">
     <div class="banner-container">
-      <img class="banner-img" src="@/assets/images/trip4.jpg" />
+      <v-carousel
+        v-if="items.length > 0"
+        cycle
+        hide-delimiter-background
+        :show-arrows="false"
+        hide-delimiters
+        style="height: 100%"
+      >
+        <v-carousel-item v-for="(item, i) in items" :key="i">
+          <v-sheet height="100%" width="100%">
+            <img class="banner-img" :src="item.thumbnailInfo?.url" />
+            <div class="text-overlay-review">
+              <div class="review-box"></div>
+              <div class="text-content-review"></div>
+            </div>
+          </v-sheet>
+        </v-carousel-item>
+        <div class="text-overlay">
+          <div class="text-content">
+            <h2 class="banner-text-left">
+              새로운 사람과 낭만 가득한 <br />여행을 떠나 볼까요?
+            </h2>
+            <br />
+            <p>MayTrip에서 여행 동반자를 찾아보세요</p>
+            <br />
+            <v-btn
+              class="banner-left-btn"
+              @click="router.push({ name: 'board-list' })"
+              >함께 여행하기 &#10149;
+            </v-btn>
+          </div>
+        </div>
+      </v-carousel>
     </div>
+
     <div class="main-wrapper">
       <div class="main-section1">
         <p class="subject-text">여행 다녀왔어요!</p>
         <br />
         <div class="card-section1-wrap">
-          <v-sheet :elevation="11" :height="300" :width="300" rounded>
-            <img class="banner-img" src="@/assets/images/trip4.jpg" />
-          </v-sheet>
-          <v-sheet :elevation="11" :height="300" :width="300" rounded></v-sheet>
-          <v-sheet :elevation="11" :height="300" :width="300" rounded></v-sheet>
+          <div v-for="(review, i) in reviews" :key="i">
+            <v-sheet :elevation="11" :height="300" :width="300" rounded>
+              <img
+                class="banner-img"
+                :src="review.thumbnailInfo?.url"
+                :alt="review.thumbnailInfo?.originFileName"
+              />
+            </v-sheet>
+          </div>
           <button>
-            <v-btn :height="300" rounded="xl">
+            <v-btn
+              :height="300"
+              rounded="xl"
+              @click="router.push({ name: 'review-list' })"
+            >
               <i class="fas fa-angle-double-right arrow"></i>
             </v-btn>
           </button>
@@ -35,109 +94,28 @@ const items = ref([
       <div class="main-section2">
         <p class="subject-text">함께 할 크루원을 찾아요</p>
         <br />
-
-        <div>
-          <v-carousel
-            cycle
-            hide-delimiter-background
-            :show-arrows="false"
-            :show-delimiters="false"
-            hide-controls
-          >
-            <v-carousel-item v-for="(item, i) in items" :key="i">
-              <v-sheet
-                class="fill-height ma-0 pa-0"
-                style="
-                  display: flex;
-                  border-radius: 10px;
-                  position: relative;
-                  overflow: hidden;
-                "
-                rounded
-              >
-                <!-- 검은색 투명도 레이어 -->
-                <div
-                  style="
-                    position: absolute;
-                    bottom: 1%;
-                    left: 0;
-                    width: 100%;
-                    height: 97%;
-                    background-color: rgba(0, 0, 0, 0.6);
-                    border-radius: 10px;
-                  "
-                ></div>
-
-                <!-- 오른쪽 이미지 -->
-                <v-img
-                  :src="items[(i + 1) % items.length].src"
-                  aspect-ratio="16/9"
-                  style="
-                    width: 60%;
-                    position: absolute;
-                    right: 0;
-                    bottom: 1%;
-                    z-index: 1;
-                    opacity: 0.5;
-                    transition: opacity 0.3s ease-in-out;
-                    object-fit: cover;
-                    border-radius: 10px;
-                  "
-                ></v-img>
-
-                <!-- 왼쪽 이미지 -->
-                <v-img
-                  :src="items[(i + items.length - 1) % items.length].src"
-                  aspect-ratio="16/9"
-                  style="
-                    width: 60%;
-                    position: absolute;
-                    left: 0;
-                    bottom: 1%;
-                    z-index: 2;
-                    opacity: 0.5;
-                    transition: opacity 0.3s ease-in-out;
-                    object-fit: cover;
-                    border-radius: 10px;
-                    height: ;
-                  "
-                ></v-img>
-
-                <!-- 중앙 이미지 -->
-                <v-img
-                  :src="item.src"
-                  aspect-ratio="16/9"
-                  style="
-                    width: 60%;
-                    position: absolute;
-                    bottom: 1%;
-                    left: 20%;
-                    z-index: 3;
-                    transition: opacity 0.3s ease-in-out;
-                    object-fit: cover;
-                    border-radius: 10px;
-                    box-shadow: 0px 0px 10px 3px #000;
-                  "
-                ></v-img>
-
-                <!-- 텍스트 -->
-                <div
-                  style="
-                    position: absolute;
-                    top: 50%;
-                    left: 50%;
-                    transform: translate(-50%, -50%);
-                    z-index: 4;
-                    color: white; /* 텍스트 색상을 흰색으로 지정 */
-                  "
-                >
-                  <span>여기는 텍스트 입니다.</span>
-                  <span>{{ item.text }}</span>
-                </div>
-              </v-sheet>
-            </v-carousel-item>
-          </v-carousel>
+        <div class="card-section1-wrap">
+          <div v-for="(item, i) in items" :key="i">
+            <v-sheet :elevation="11" :height="300" :width="300" rounded>
+              <img
+                class="banner-img"
+                :src="item.thumbnailInfo?.url"
+                :alt="item.thumbnailInfo?.originFileName"
+              />
+            </v-sheet>
+          </div>
+          <button>
+            <v-btn
+              :height="300"
+              rounded="xl"
+              @click="router.push({ name: 'board-list' })"
+            >
+              <i class="fas fa-angle-double-right arrow"></i>
+            </v-btn>
+          </button>
         </div>
+
+        <br />
       </div>
     </div>
   </div>
@@ -165,6 +143,49 @@ const items = ref([
   width: 100%;
   height: 100%;
 }
+
+.banner-text-left {
+  font-size: 30px;
+  text-shadow: 2px 6px 2px gray;
+}
+.banner-left-btn {
+  box-shadow: 0px 8px 15px 0px rgba(0, 0, 0, 0.3);
+}
+.banner-left-btn:hover {
+  transform: translateY(15px);
+  /* opacity: 0.3; */
+  background-color: #f2e3db;
+  color: black;
+}
+.text-overlay {
+  position: absolute;
+  top: 50%;
+  left: 30%;
+  transform: translate(-50%, -50%);
+  /* text-align: center; */
+  color: white;
+  z-index: 2;
+}
+.text-content {
+  max-width: 100%;
+}
+
+.text-overlay-review {
+  position: absolute;
+  top: 50%;
+  right: 15%;
+  transform: translate(-50%, -50%);
+  /* text-align: center; */
+  color: white;
+  z-index: 2;
+}
+.review-box {
+  opacity: 0.6;
+  background-color: black;
+  width: 300px;
+  height: 400px;
+  border-radius: 10px;
+}
 .main-wrapper {
   margin-top: calc(70px + 700px); /* header + banner */
 }
@@ -179,16 +200,6 @@ const items = ref([
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-}
-.card-section1-wrap .item {
-}
-.card-section1-wrap img {
-  border-radius: 5px;
-}
-.card-section1-wrap button {
-}
-.main-section2 {
-  margin-bottom: 100px;
 }
 
 .arrow {
